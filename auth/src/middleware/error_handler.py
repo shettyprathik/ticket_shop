db737a2726e739711510ad64852c3848d75d6ac1
prathik_shetty <prathik.gunapal@gmail.com>
2020-09-05 16:54:52 +0530

    mongo pip and error handle schema name updated

--- auth/src/middleware/error_handler.py
+++ auth/src/middleware/error_handler.py
@@ -3,15 +3,18 @@ from flask import jsonify
 from pydantic import BaseModel
 from typing import List, Optional
 
-class ErrorSingle(BaseModel):
+
+class ErrorSingleSchema(BaseModel):
     message: str
     field: Optional[str]
 
-class ErrorList(BaseModel):
-    errors: List[ErrorSingle]
+
+class ErrorListSchema(BaseModel):
+    errors: List[ErrorSingleSchema]
+
 
 @app.errorhandler(Exception)
 def handle_error(err):
     serialize_errors = err.serialize_errors()
-    ErrorList(errors=serialize_errors)
-    return jsonify(serialize_errors)
+    error_out = ErrorListSchema(errors=serialize_errors).dict()['errors']
+    return jsonify(error_out)
